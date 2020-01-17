5d2481fbbb2634a6fdc5f4e1e90834e21877df93
Scott Feister <sfeister@gmail.com>
2020-01-17 08:52:05 -0800

    Small edits to midisound test take three

--- troubleshoot/test_midisound.py
+++ troubleshoot/test_midisound.py
@@ -4,6 +4,7 @@
 # Python script for catching Ctrl + C copied from https://www.devdungeon.com/content/python-catch-sigint-ctrl-c
 
 from signal import signal, SIGINT
+import subprocess
 from sys import exit
 from time import sleep
 import fluidsynth # sudo apt install fluidsynth; sudo pip3 install pyFluidSynth
@@ -24,6 +25,10 @@ if __name__ == '__main__':
 
     sfid = fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
     fs.program_select(0, sfid, 0, 0)
+
+    # Make the jack audio connections of fluidsynth outputs to enable system playback
+    subprocess.call(["jack_connect", "fluidsynth:l_00", "system:playback_1"])
+    subprocess.call(["jack_connect", "fluidsynth:r_00", "system:playback_2"])
 
     print("Playing twelve notes in a row, using MIDI! You should hear them.")
     print("Press Ctrl + C to quit early.")
