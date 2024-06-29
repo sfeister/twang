849702454b2ea8a7fd63c7b80eff610d21724bf3
Scott Feister <sfeister@gmail.com>
2024-06-28 17:55:31 -0700

    Added the example harp.py file for the Pi Pico

--- examples/harp.py
+++ examples/harp.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-harp.py: Ten-string laser harp with MIDI output
-
-Start your instrument via "python3 harp.py"
+harp.py: Ten-string laser harp with MIDI output, for Pi Pico
 
 Notes:
- * Valid pins are, in left-to-right order along the Pi Stereo Bonnet: 4,17,27,22,25,5,6,12,13,16,20
-     ** (Reverse order: 20,16,13,12,6,5,25,22,27,17,4)
  * The MIDI note codes for nine notes in a major scale (C to C) are: 60,62,64,65,67,69,71,72,73
 
-Created by Scott Feister on January 9, 2020
+Created by Scott Feister on June 28, 2024
 """
 
 from twang import LightString, LightInstrument
+import board
 
 if __name__ == "__main__":
     ## Define your ten strings (low-note strings first)
     lstrings = [None]*10
-    lstrings[0] = LightString(pin=20, midinote=60)
-    lstrings[1] = LightString(pin=16, midinote=62)
-    lstrings[2] = LightString(pin=13, midinote=64)
-    lstrings[3] = LightString(pin=12, midinote=65)
-    lstrings[4] = LightString(pin=6, midinote=67)
-    lstrings[5] = LightString(pin=5, midinote=69)
-    lstrings[6] = LightString(pin=25, midinote=71)
-    lstrings[7] = LightString(pin=22, midinote=72)
-    lstrings[8] = LightString(pin=27, midinote=73)
-    lstrings[9] = LightString(pin=17, midinote=75)
+    lstrings[0] = LightString(pin=board.GP10, midinote=60)
+    lstrings[1] = LightString(pin=board.GP11, midinote=62)
+    lstrings[2] = LightString(pin=board.GP12, midinote=64)
+    lstrings[3] = LightString(pin=board.GP13, midinote=65)
+    lstrings[4] = LightString(pin=board.GP14, midinote=67)
+    lstrings[5] = LightString(pin=board.GP15, midinote=69)
+    lstrings[6] = LightString(pin=board.GP16, midinote=71)
+    lstrings[7] = LightString(pin=board.GP17, midinote=72)
+    lstrings[8] = LightString(pin=board.GP18, midinote=73)
+    lstrings[9] = LightString(pin=board.GP19, midinote=75)
     
     ## Combine the strings together into a playable instrument!
-    myharp = LightInstrument(lstrings, midi_instrument=8, gain=1.0)
-    myharp.start()
+    myharp = LightInstrument(lstrings, beampin=board.GP2)
+    myharp.run()
