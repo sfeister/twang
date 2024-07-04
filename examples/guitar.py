ef2d7c48390a9525d9b7312bd33ea8d732508e3e
Scott Feister <sfeister@gmail.com>
2024-07-03 17:28:26 -0700

    Wonderful and working great!

--- examples/guitar.py
+++ examples/guitar.py
@@ -12,8 +12,8 @@ import ulab.numpy as np
 import board
 
 # Standard guitar tuning: E2–A2–D3–G3–B3–E4
-opennames = ["E2","A2","D3","G3","B3","E4"]
-opennotes = [getnote(name) for name in opennames]
+OPEN_NAMES = ["E2","A2","D3","G3","B3","E4"]
+OPEN_NOTES = [getnote(name) for name in OPEN_NAMES]
 
 
 ### RELATIVE CHORDS (based on frets)
@@ -50,30 +50,34 @@ Gm = [3,5,5,3,3,3]
 # More major chords
 B = [x,2,4,4,4,2]
 
-def midify(relnotes):
-    """ Convert relative chord to an absolute chord in midi
-    Relies on variables opennotes and x defined above"""
-    notes = np.array(relnotes, dtype=np.int16) + np.array(opennotes, dtype=np.int16)
-    notes[notes < 0] = -9999 # Reset unpluckable strings to -9999
-    return notes
+def shift_notes(notes, shifts):
+    """ Shift a list of notes by a number of half-steps """
+    # TODO: Check assertions
+    shifted_notes = np.array(notes, dtype=np.int16) + np.array(shifts, dtype=np.int16) # Add the shift in note onto the note itself
+    return shifted_notes
     
 if __name__ == "__main__":
-    # Button pins for chords, and their associated chords
-    cbuttons = [None]*4
-    cbuttons[0] = ChordButton(pin=board.GP20, midinotes=midify(C))
-    cbuttons[1] = ChordButton(pin=board.GP21, midinotes=midify(G))
-    cbuttons[2] = ChordButton(pin=board.GP22, midinotes=midify(Am))
-    cbuttons[3] = ChordButton(pin=board.GP23, midinotes=midify(F))
+    print("OPEN_NOTES (midi numbers for an open chord): {}".format(OPEN_NOTES))
     
     # Phototransistor pins for strings (low-note strings first)
-    lstrings = [None]*6
-    lstrings[0] = LightString(pin=board.GP11, midinote=opennotes[0])
-    lstrings[1] = LightString(pin=board.GP12, midinote=opennotes[1])
-    lstrings[2] = LightString(pin=board.GP13, midinote=opennotes[2])
-    lstrings[3] = LightString(pin=board.GP14, midinote=opennotes[3])
-    lstrings[4] = LightString(pin=board.GP15, midinote=opennotes[4])
-    lstrings[5] = LightString(pin=board.GP16, midinote=opennotes[5])
+    STRINGS = [
+        LightString(pin=board.GP10, note=OPEN_NOTES[0]),
+        LightString(pin=board.GP11, note=OPEN_NOTES[1]),
+        LightString(pin=board.GP12, note=OPEN_NOTES[2]),
+        LightString(pin=board.GP13, note=OPEN_NOTES[3]),
+        LightString(pin=board.GP14, note=OPEN_NOTES[4]),
+        LightString(pin=board.GP15, note=OPEN_NOTES[5]),
+    ]
     
+    # Button pins for chords, and their associated notes
+    CHORD_BTNS = [
+        ChordButton(pin=board.GP16, notes=shift_notes(OPEN_NOTES, C)),
+        ChordButton(pin=board.GP17, notes=shift_notes(OPEN_NOTES, G)),
+        ChordButton(pin=board.GP18, notes=shift_notes(OPEN_NOTES, Am)),
+        ChordButton(pin=board.GP19, notes=shift_notes(OPEN_NOTES, F)),
+        ChordButton(pin=board.GP20, notes=shift_notes(OPEN_NOTES, Em)),
+    ]
+
     # Combine the buttons and strings together into an instrument!
-    myguitar = LightInstrument(lstrings, chordbtns=cbuttons, beampin=board.GP2)
+    myguitar = LightInstrument(strings=STRINGS, chord_btns=CHORD_BTNS, beam_pin=board.GP2, midi_program=1)
     myguitar.run()
--- twang/instrument.py
+++ twang/instrument.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-instrument.py: Class-based definitions of light-based instruments on Raspberry Pi Pico microcontrollers.
+instrument.py: Object-oriented approach to light-based instruments on Raspberry Pi Pico microcontrollers.
 
-Outputs MIDI, one string per channel.
+Example "Light Instruments" you can build with this library:
+* Laser/LED Harp
+* Laser/LED Banjo
+* Laser/LED Ukelele
+* Laser/LED Guitar
 
-Watches for plucks of light strings and pressing of buttons to change chords.
-Supports use of buttons to change chords ,facilitating not just laser harps, but laser guitars!
+In Twang, a "LightInstrument" polls for plucks or strums of its "LightStrings" and sends MIDI messages to play sounds accordingly.
+Optionally, you can add "ChordButtons" which, while depressed, temporarily shift the notes of the "LightStrings".
 
 For use with the Pi Pico and CircuitPython.
-
-TODO:
-    * Facilitate easier changing of instrument
-    
+  
 Created by Scott Feister June 28, 2024.
 """
 
@@ -21,12 +22,11 @@ import ulab.numpy as np
 import board
 import keypad
 from digitalio import DigitalInOut, Direction, Pull
-import pulseio
 import usb_midi
-
 import adafruit_midi
 from adafruit_midi.note_off import NoteOff
 from adafruit_midi.note_on import NoteOn
+from adafruit_midi.program_change import ProgramChange
 
 class LightInstrument:
     """
@@ -36,22 +36,27 @@ class LightInstrument:
     Example: A laser harp, an LED-based piano, or a laser guitar.
     """
 
-    def __init__(self, lstrings, open_chord=None, chordbtns=None, beampin=board.GP2):
-        """ If open_chord is specified, overrides the lstring values """
+    def __init__(self, strings, open_chord=None, chord_btns=None, beam_pin=board.GP2, midi_program=None, midi_channel=0, debug=False):
+        """ If open_chord is specified, overrides the string values """
 
-        self.nstrings = len(lstrings) # Count the number of strings
-        self.lstrings = lstrings # A list of LightString objects
-        self.chordbtns = chordbtns # A list of ChordButton objects; if None, assume this is a harp-like instrument (no chord changes)
-        self.beampin = beampin # The GPIO output pin that controls the light source for the strings (e.g. the pin that controls the lasers)
+        self.debug = debug
+        self.num_strings = len(strings) # Count the number of strings
+        self.strings = strings # A list of LightString objects
+        self.chord_btns = chord_btns # A list of ChordButton objects; if None, assume this is a harp-like instrument (no chord changes)
+        self.beam_pin = beam_pin # The GPIO output pin that controls the light source for the strings (e.g. the pin that controls the lasers)
                     
         # Set up the light source pin as an output
-        self.beam = DigitalInOut(self.beampin)
+        self.beam = DigitalInOut(self.beam_pin)
         self.beam.direction = Direction.OUTPUT
         
-        self.midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
-        for lstring in lstrings:
-            if not lstring.midi:
-                lstring.midi = self.midi
+        self.midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=midi_channel)
+        
+        if midi_program is not None: # if unset, leave the midi program selection alone
+            self.midi.send(ProgramChange(midi_program, channel=midi_channel))
+        
+        for string in strings:
+            if not string.midi:
+                string.midi = self.midi
         
         # Do a little blinky show
         for i in range(4):
@@ -60,82 +65,84 @@ class LightInstrument:
             self.beam.value = False
             sleep(0.1)
 
-        # Initialize notes on the lstrings with an open chord
+        # Initialize notes on the strings with an open chord
         if open_chord is None:
-            self.open_chord = [lstring.note for lstring in lstrings] # The default chord is pulled from the strings as configured right now
+            self.open_chord = [string.note for string in strings] # The default chord is pulled from the strings as configured right now
         else:
             self.open_chord = open_chord
         
-        self.update_chord(self.open_chord)
+        self.update_notes(self.open_chord)
         
-        if chordbtns is not None:
-            # Check that there are equal number of notes in all chords as there are lstrings!
-            if len(self.open_chord) != len(lstrings):
+        if chord_btns is not None:
+            # Check that there are equal number of notes in all chords as there are strings!
+            if len(self.open_chord) != len(strings):
                 raise Exception("Number of notes in the open_chord does not match the number of LightStrings in the LightInstrument!")
             else:
-                for chordbtn in chordbtns:
-                    if len(chordbtn.notes) != len(lstrings):
+                for chord_btn in chord_btns:
+                    if len(chord_btn.notes) != len(strings):
                         raise Exception("Number of notes in ChordButton's chord does not match the number of LightStrings in the LightInstrument!")                
 
-            # Initialize chord buttons array to all False (nothing pressed)
-            self.chordarr = np.array([False for btn in self.chordbtns]) # A True/False list of whether chord buttons are pressed
+            # Initialize chord activation array to all False (nothing pressed)
+            self.chord_status = np.array([False for btn in self.chord_btns]) # A True/False list of whether chord buttons are pressed
         
     def run(self):
         """ Begins endless loop of the instrument """           
         # Play an intro diddy
-        self.lstrings[0].play()
+        self.strings[0].play()
 
         print("Instrument starting, ready to play!")
         self.beam.value = True # Turn on light source for the strings (e.g. turn on the lasers)
-
-        #for lstring in self.lstrings:
-        #    lstring.start()
             
         while True:
-            # Update the chord as needed
-            self.check_and_update_chord() # TODO
+            # Check for chord changes, and if so,
+            #     update strings' notes
+            self.check_for_chord_change()
             
-            # Send MIDI signal for any strings that have been plucked since last loop iteration
-            for lstring in self.lstrings:
-                lstring.check_and_play()
+            # Check if any strings have been plucked,
+            #      and if so, send midi messages
+            for string in self.strings:
+                string.check_and_play()
     
-    def update_chord(self, chord):
-        """ Update the currently implemented chord """
-        for note, lstring in zip(chord, self.lstrings):
-            lstring.change_note(note)
-        
-        self.chord = chord
-                        
-    def check_and_update_chord(self):
+    def update_notes(self, notes):
+        """ Update the instrument's notes, e.g. when shifting chords """           
+        for note, string in zip(notes, self.strings):
+            string.change_note(note)
+                                
+    def check_for_chord_change(self):
         """ Update the currently implemented chord, only if new button has been pressed """
-        if self.chordbtns is not None:
-            chordarr = np.array([btn.is_pressed() for btn in self.chordbtns]) # List of True/False on whether buttons are pressed
-            if np.sum((chordarr - self.chordarr)**2) > 0:
+        if self.chord_btns is not None:
+            chord_status = np.array([btn.is_pressed() for btn in self.chord_btns]) # List of True/False on whether buttons are pressed
+            if np.sum((chord_status - self.chord_status)**2) > 0:
                 # Update the chord array for future comparison
-                self.chordarr = chordarr
+                self.chord_status = chord_status
     
                 # Find the new chord notes
-                if not np.any(chordarr):
-                    chord = self.open_chord # No buttons pressed; use the open chord
+                if not np.any(chord_status):
+                    notes = self.open_chord # No buttons pressed; use the open chord nontes
+                    if self.debug:
+                        print("Changing to open chord.")
                 else:
-                    ix = np.argmax(chordarr) # Index of the depressed chord (first occurrence), if applicable
-                    chord = self.chordbtns[ix].notes
-                    
-                # Apply the new chord notes to the lstrings
-                self.update_chord(chord)
+                    ix = np.argmax(chord_status) # Index of the depressed chord (first occurrence), if applicable
+                    notes = self.chord_btns[ix].notes
+                    if self.debug:
+                        print("Changing to chord #{}.".format(ix))
+
+                # Apply the new chord notes to the strings
+                self.update_notes(notes)
                                 
 class ChordButton:
     """
-    Class describing a pushbutton that, when pressed, changes the chord
+    A single chord and the button that activates it.
+    Optional for LightInstruments.
     
-    -9999 is the only note allowed outside the integer range for midi notes.
+    Midi note values less than -1 are ignored in Twang.
     """
-    def __init__(self, pin, midinotes):
-        self.notes = midinotes # A list of midi notes in this chord. E.g. notes = [12, 18, 19, 30, 41]. Should match number of strings. 
-        notes_set = set(midinotes)
+    def __init__(self, pin, notes, debug=False):
+        self.debug = debug
         
-        if -9999 in notes_set:
-            notes_set.remove(-9999) # -9999 is a placeholder for non-pluckable string
+        self.notes = notes # A list of midi notes in this chord. E.g. notes = [12, 18, 19, 30, 41]. Should match number of strings. 
+
+        notes_set = set([x for x in notes if x > -1]) # Exclude negative numbers, as these are assumed to be unusable later in the code
             
         if not notes_set.issubset(set(range(128))): # Check that notes list contains only valid midi notes
             raise Exception("Invalid notes. All midi notes must be integers the range of 0 to 127.")
@@ -152,16 +159,17 @@ class ChordButton:
 
 class LightString:
     """
-    Class describing a single beam of light and its interrupt
-       
+    A single string in the LightInstrument
+    
+    Includes the beam of light, photodetector, and its assigned midi note
     """
     
-    def __init__(self, pin, midinote=72, midi=None):
+    def __init__(self, pin, note=72, midi=None, debug=False):
+        self.debug = debug
         self.pin = pin # "board" pin number for phototransistor
         self.midi = midi # "adafruit_midi" MIDI instance (if left as None, nothing will play)
-        self.note = midinote # MIDI note
+        self.note = note # MIDI note
         self.last_note = self.note
-        #self.pulses = pulseio.PulseIn(self.pin, maxlen=10, idle_state=False)
         self.keys = keypad.Keys((self.pin,), value_when_pressed=False, pull=True, interval=0.01)
         
         self.xp = np.logspace(np.log10(50), np.log10(500), num=20) # for interpolating pluck duration
@@ -179,43 +187,31 @@ class LightString:
         if self.note > -1: # Exclude case of -9999, which we are using to represent an unpluckable string
             msg_note_off = NoteOff(self.last_note) # Stop playing old sound
             msg_note_on = NoteOn(self.note, velocity=velocity) # Play new sound
-            self.last_note = self.note # Update the last_note variable
             self.midi.send([msg_note_off, msg_note_on])
+
+            self.last_note = self.note # Update the last_note variable
             
-    # Use pulse logic for getting a velocity on the strum
     def check_and_play(self):
-        event = self.keys.events.get()
-        # event will be None if nothing has happened.
-        if event:
+        event = self.keys.events.get() # event will be None if nothing has happened
+        if event: # a string has either been pressed or released
             if event.pressed:
-                self.pressed_ticks_ms = event.timestamp
+                # When string is pressed, mark down a timestamp and remain silent
+                self.pressed_ticks_ms = event.timestamp  # timestamp, in milliseconds, for the press of the string
             if event.released:
+                # When string is released, sound the note at a volume proportional to the time elapsed between press and release
                 self.keys.events.clear()
-                pluck_ms = event.timestamp - self.pressed_ticks_ms
-                velocity = int(np.interp([pluck_ms], self.xp, self.yp)[0]) # Scale it down
-                self.play(velocity=velocity) # TODO: Add in a velocity
-                print("Pluck detected!") # DEBUG
-                print("Pluck duration (ms): {}".format(pluck_ms)) # DEBUG
-                print("Pluck velocity (0-127): {}".format(velocity)) # DEBUG
-
-        # if len(self.pulses) > 0: # Check for a new pluck of this string
-            # print("Pluck detected!") # DEBUG
-            
-            # self.pulses.pause()
-            # npulses = len(self.pulses)
-            # pulse_arr = np.zeros(npulses)
-            # for i in range(len(self.pulses)):
-                # pulse_arr[i] = self.pulses[i]
-
-            # self.pulses.clear()
-            # self.pulses.resume()
-            # print("List of raw pulses (us): {}".format(pulse_arr)) # DEBUG
-            # longest_active_us = np.max(pulse_arr[::2])
-            # print("Pluck duration (us): {}".format(longest_active_us)) # DEBUG
-            
-            # # Play the pluck as midi, with corresponding velocity
-            # self.play() # TODO: Add in a velocity
-
+                released_ticks_ms = event.timestamp # timestamp, in milliseconds, for the release of the string
+                pluck_ms = released_ticks_ms - self.pressed_ticks_ms # The duration of the pluck is the millisecond difference between the press and release of the string
+                # Note: unhandled overflow can occur here (empirically seen infrequently)
+                
+                # Scale the MIDI note velocity by the duration of the pluck. Plucking faster will make a louder sound.
+                velocity = int(np.interp([pluck_ms], self.xp, self.yp)[0]) # Scale the velocity (arbitrary units of 0-127) to the pluck duration (milliseconds) using an interpolation function
+                
+                self.play(velocity=velocity) # play the sound by sending a MIDI message
+                if self.debug:
+                    print("Pluck detected!")
+                    print("Pluck duration (ms): {}".format(pluck_ms))
+                    print("Pluck velocity (0-127): {}".format(velocity))
 
 if __name__ == "__main__":
     pass
